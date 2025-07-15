2c60c5f47ce6934a173f61f349c2bc79a28f5029
Marcos Corrochano Arroyo <mcorrochanoarroyo@gmail.com>
2025-07-15 16:57:45 +0200

    second commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,23 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import tailwindcss from '@tailwindcss/vite';
+import icon from 'astro-icon';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://mc-8.github.io/',
+  integrations: [sitemap(), icon()],
+  experimental: {
+    fonts: [
+      {
+        cssVariable: '--be-vietnam-pro',
+        name: 'Be Vietnam Pro',
+        provider: fontProviders.fontsource()
+      }
+    ]
+  },
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
